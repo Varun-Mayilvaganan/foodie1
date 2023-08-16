312239c3b0c517cca56cbb117ec9d7a86ec15fc3
Varun <varunmayilvaganan11@gmail.com>
2023-08-16 20:57:51 +0530

    updated all

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:foodie/utils/colors.dart';
+import 'package:foodie/widgets/small_text.dart';
+import '../widgets/big_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top:20,bottom: 15),
+              padding: EdgeInsets.only(left: 20,right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+
+                      BigText(text:"India",
+                      color: AppColors.Locate,
+                      ),
+
+                      SmallText(text: "Ranipet", color: Colors.black54)
+                    ],
+                  ),
+                  Container(
+
+                    width: 45,
+                    height: 45,
+                    child: IconButton(
+                      onPressed: (){}, 
+                      icon: Icon(Icons.search,
+                      color: Colors.black),
+                      ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
+
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
