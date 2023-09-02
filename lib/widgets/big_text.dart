f2251692151ee26a3b45a2064125d97bdeb2f1b8
Varun Mayilavaganan <112771701+Varun-Mayilvaganan@users.noreply.github.com>
2023-09-02 20:53:23 +0530

    Update big_text.dart

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 
 class BigText extends StatelessWidget {
-  final Color color;
-  final String text;
-  final double size;
-  final TextOverflow overflow;
+  final Color color;             // Text color, default is a dark brown
+  final String text;             // Text content
+  final double size;             // Font size, default is 22
+  final TextOverflow overflow;   // Text overflow behavior, default is ellipsis
 
-  const BigText({Key? key,this.color = const Color(0xff332d2b),
-    required this.text,
-    this.size=22,
-    this.overflow=TextOverflow.ellipsis
-  }):super(key:key);
+  // Constructor for the BigText widget
+  const BigText({
+    Key? key,                    // Key for widget identification
+    this.color = const Color(0xff332d2b),  // Default color
+    required this.text,           // Required text content
+    this.size = 22,               // Default font size
+    this.overflow = TextOverflow.ellipsis   // Default overflow behavior
+  }) : super(key: key);           // Call the super constructor
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      text,
-      overflow: overflow,
+      text,                        // Display the provided text
+      overflow: overflow,         // Apply the specified overflow behavior
       style: TextStyle(
-        fontFamily: 'Roboto',
-        color:color,
-        fontSize: size,
-        fontWeight: FontWeight.w600,
+        fontFamily: 'Roboto',     // Font family for the text
+        color: color,             // Apply the specified text color
+        fontSize: size,           // Apply the specified font size
+        fontWeight: FontWeight.w600,  // Apply font weight (semi-bold)
       ),
-
     );
   }
 }
